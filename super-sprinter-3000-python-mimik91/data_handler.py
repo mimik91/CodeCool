520325e2fea520b1121abcf1339c98113ab5e5a7
Dominik Lach <dominiklach@poczta.fm>
2020-07-01 15:06:27 +0200

    aktualizacja

--- super-sprinter-3000-python-mimik91/data_handler.py
+++ super-sprinter-3000-python-mimik91/data_handler.py
@@ -3,8 +3,20 @@ import os
 
 DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'data.csv'
 DATA_HEADER = ['id', 'title', 'user_story', 'acceptance_criteria', 'business_value', 'estimation', 'status']
-STATUSES = ['planning', 'todo', 'in progress', 'review', 'done']
+FILE_PATH='/home/dominik/Desktop/CodeCool/super-sprinter-3000-python-mimik91/test/data.csv'
 
 
 def get_all_user_story():
-    return []
+    with open(FILE_PATH, 'rt')as f:
+        stories=csv.reader(f)
+        stories_dic={}
+        n=0
+        for row in stories:
+            stories_dic[n]=row
+            n=n+1
+        del stories_dic[0]
+        return stories_dic
+
+
+
+get_all_user_story()
--- super-sprinter-3000-python-mimik91/server.py
+++ super-sprinter-3000-python-mimik91/server.py
@@ -2,24 +2,37 @@ from flask import Flask, render_template, request, redirect, url_for
 
 import data_handler
 
+USER_STORY = 2
+ACCEPTANCE_CRITERIA = 3
+
+
 app = Flask(__name__)
 DATA_HEADER=data_handler.DATA_HEADER
-STATUSES=data_handler.STATUSES
 
-DATA = {1:[1, "Pszczółka Maja", "Z piosenką Zbysza Wodeckkiego", "No", "6789", 3000, "planing"],
-        2:[2, "Scooby Doo", "Z Cartoon-Netwoork po angielsku", "Yes", "6789", 3000, "planing"],
-        3:[3, "Scooby Doo", "Z Cartoon-Netwoork po angielsku", "Yes", "6789", 3000, "planing"]}
+def convert_linebreaks_to_br(original_str):
+    return ' '.join(original_str.split('<br>'))
 
-@app.route('/test')
-def test():
+#DATA = data_handler.DATA
+
+@app.route('/')
+@app.route('/list')
+def route_list():
     user_stories = data_handler.get_all_user_story()
+    print(user_stories)
+    for story in user_stories:
+        user_stories[story][USER_STORY] = convert_linebreaks_to_br(user_stories[story][USER_STORY])
+        user_stories[story][ACCEPTANCE_CRITERIA] = convert_linebreaks_to_br(user_stories[story][ACCEPTANCE_CRITERIA])
+    return render_template('list.html', DATA_HEADER=DATA_HEADER, user_stories=user_stories)
 
-    return render_template('test.html', user_stories=user_stories, DATA_HEADER=DATA_HEADER)
+@app.route('/story')
+def edit_list():
+    user_stories = data_handler.get_all_user_story()
+    return render_template('edit.html', user_stories=user_stories, DATA_HEADER=DATA_HEADER, story_id=None, last_id=max(user_stories.keys()))
 
 @app.route('/story/<story_id>')
 def edit_data(story_id):
     user_stories = data_handler.get_all_user_story()
-    return render_template('edit.html', user_stories=user_stories, DATA_HEADER=DATA_HEADER, story=DATA[int(story_id)])
+    return render_template('edit.html', user_stories=user_stories, DATA_HEADER=DATA_HEADER, story=user_stories[int(story_id)])
 
 @app.route('/post/<new_id>', methods=['POST'])
 def post_data(new_id):
@@ -32,21 +45,6 @@ def post_data(new_id):
     new_story=(new_id, title, story_text,acceptance, bussines, estimation, status)
     DATA[int(new_story[0])]=new_story
     return redirect(url_for('route_list'))
-
-
-
-
-@app.route('/')
-@app.route('/list')
-def route_list():
-    user_stories = data_handler.get_all_user_story()
-    return render_template('list.html', DATA_HEADER=DATA_HEADER, DATA=DATA)
-
-@app.route('/story')
-def edit_list():
-    user_stories = data_handler.get_all_user_story()
-    return render_template('edit.html', user_stories=user_stories, DATA_HEADER=DATA_HEADER, story_id=None, last_id=max(DATA.keys()))
-
 
 if __name__ == '__main__':
     app.run(
